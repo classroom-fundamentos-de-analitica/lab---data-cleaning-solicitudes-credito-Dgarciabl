c4f651be46d15a2fcd36cee86bf3d09e4f1f8a31
Dgarciabl <55594672+Dgarciabl@users.noreply.github.com>
2022-11-21 02:17:22 -0500

    final

--- pregunta.py
+++ pregunta.py
@@ -6,8 +6,10 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
 
+import pandas as pd
+import numpy as np
+from datetime import datetime
 
 def clean_data():
 
@@ -17,4 +19,33 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    df.drop(['Unnamed: 0'], axis=1,inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis=0,inplace=True)
+    
+    #Pre-procesamiento
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: x.replace("$ ",''))
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: x.replace(",",''))
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: x.replace(".00",''))
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float)
+    df['tipo_de_emprendimiento']=df['tipo_de_emprendimiento'].astype(str)
+    df['tipo_de_emprendimiento']=df['tipo_de_emprendimiento'].str.lower()
+    df['sexo']=df['sexo'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].map(lambda x: x.replace(" ",'_'))
+    df['idea_negocio'] = df['idea_negocio'].map(lambda x: x.replace("-",'_'))
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    df['barrio'] = df['barrio'].astype(str)
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].map(lambda x: x.replace("-",' '))
+    df['barrio'] = df['barrio'].map(lambda x: x.replace("_",' '))
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y'))
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime('%d/%m/%Y')
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].map(lambda x: x.replace(" ",'_'))
+    df['línea_credito'] = df['línea_credito'].map(lambda x: x.replace("-",'_'))
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis=0, inplace=True)
+    
     return df
